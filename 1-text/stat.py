86f1f0358db13815f57064586460ae35dca842ee
Martin Galajda <433487@mail.muni.cz>
2018-09-25 09:49:40 +0200

    Final fixes (for python version 2.7 bc of problems with encoding

--- 1-text/stat.py
+++ 1-text/stat.py
@@ -1,7 +1,6 @@
 import sys
 import re
 
-
 if len(sys.argv) != 3:
   raise ValueError("Program expects 3 arguments")
 
@@ -11,7 +10,9 @@ option = sys.argv[2]
 regexps = {
   'composer': re.compile(r'Composer:\s(.*)$'),
   'composition_year': re.compile(r'Composition Year:\s(\d+)'),
-  'composer_extra_stuff_2': re.compile(r'\(.*\)'),
+  'composition_year_explicit_century': re.compile(r'Composition Year:\s([\d]{1,4})th'),
+  'composition_year_extra_stuff': re.compile(r'[\d]{1,2}\.\s'),
+  'composer_extra_stuff_2': re.compile(r'\(.*?\)'),
   'composer_extra_stuff_1': re.compile(r'Composer:\s'),
 }
 
@@ -36,12 +37,11 @@ def safe_print_str(str):
 def read_file():
   lines = []
 
-  with open(filename, 'r', errors='ignore') as f:
+  with open(filename, 'r') as f:
     read = 0
     for line in f:
       read += 1
       lines += [line]
-
 
   return lines
 
@@ -51,6 +51,7 @@ def parse_composers(composers_line):
   str_composers = re.sub(regexps['composer_extra_stuff_1'], '', composers_line)
   clean_composers = []
   parsed_composers = re.sub(regexps['composer_extra_stuff_2'], '', str_composers).split(';')
+
   for parsed_composer in parsed_composers:
     if len(parsed_composer.strip()) > 0:
       clean_composers += [parsed_composer.strip()]
@@ -61,9 +62,16 @@ def century_from_year(year):
   return (year) // 100 + 1    # 1 because 2017 is 21st century, and 1989 = 20th century
 
 def parse_composition_year(composition_year_line):
-  composition_year = regexps['composition_year'].match(composition_year_line).group(1)
+  preprocessed_year_line = regexps['composition_year_extra_stuff'].sub('', composition_year_line)
 
-  return int(composition_year.strip())
+  explicit_century_match = regexps['composition_year_explicit_century'].match(preprocessed_year_line)
+  if explicit_century_match:
+    return int(explicit_century_match.group(1))
+
+  composition_year = regexps['composition_year'].match(preprocessed_year_line).group(1)
+
+  year = int(composition_year.strip())
+  return century_from_year(year)
 
 lines = read_file()
 
@@ -82,7 +90,7 @@ for line in lines:
 
 
   if match_composition_year:
-    century = century_from_year(parse_composition_year(line))
+    century = parse_composition_year(line)
     if not century in stats['century'].keys():
       stats['century'][century] = 1
     else:
